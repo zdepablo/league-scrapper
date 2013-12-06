0bfc5c3288d1363cc06b2433847ed22fad6ae28c
César de Pablo <zesar.depablo@gmail.com>
2013-12-07 00:42:29 +0100

    added leagues

--- football/football/spiders/rf.py
+++ football/football/spiders/rf.py
@@ -8,7 +8,95 @@ class RFSpider(CrawlSpider):
     name = "rf"
     allowed_domains = ["resultados-futbol.com"]
     start_urls = [
-        "http://www.resultados-futbol.com/premier2013/grupo1/jornada1"
+#        "http://www.resultados-futbol.com/premier2013/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/premier2012/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/premier2010/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/premier2009/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/premier2008/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/premier2007/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/premier2006/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/premier2005/grupo1/jornada1"  
+
+
+#       "http://www.resultados-futbol.com/primera2013/grupo1/jornada1",
+#	"http://www.resultados-futbol.com/primera2012/grupo1/jornada1",
+#	"http://www.resultados-futbol.com/primera2010/grupo1/jornada1",
+#	"http://www.resultados-futbol.com/primera2009/grupo1/jornada1",
+#	"http://www.resultados-futbol.com/primera2008/grupo1/jornada1",
+#	"http://www.resultados-futbol.com/primera2007/grupo1/jornada1",
+#	"http://www.resultados-futbol.com/primera2006/grupo1/jornada1",
+#	"http://www.resultados-futbol.com/primera2005/grupo1/jornada1"  
+
+#        "http://www.resultados-futbol.com/serie_a2013/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/serie_a2012/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/serie_a2010/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/serie_a2009/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/serie_a2008/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/serie_a2007/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/serie_a2006/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/serie_a2005/grupo1/jornada1"  
+
+
+#        "http://www.resultados-futbol.com/2013/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/bundesliga2012/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/bundesliga2010/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/bundesliga2009/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/bundesliga2008/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/bundesliga2007/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/bundesliga2006/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/bundesliga2005/grupo1/jornada1"  
+
+
+#        "http://www.resultados-futbol.com/ligue_12013/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/ligue_12012/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/ligue_12010/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/ligue_12009/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/ligue_12008/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/ligue_12007/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/ligue_12006/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/ligue_12005/grupo1/jornada1"  
+
+
+#        "http://www.resultados-futbol.com/portugal2013/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/portugal2012/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/portugal2010/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/portugal2009/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/portugal2008/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/portugal2007/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/portugal2006/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/portugal2005/grupo1/jornada1"  
+
+
+#        "http://www.resultados-futbol.com/holanda2013/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/holanda2012/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/holanda2010/grupo1/jornada1",c
+#		"http://www.resultados-futbol.com/holanda2009/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/holanda2008/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/holanda2007/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/holanda2006/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/holanda2005/grupo1/jornada1"  
+
+
+#        "http://www.resultados-futbol.com/turquia2013/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/turquia2012/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/turquia2010/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/turquia2009/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/turquia2008/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/turquia2007/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/turquia2006/grupo1/jornada1",
+#		"http://www.resultados-futbol.com/turquia2005/grupo1/jornada1"  
+
+
+        "http://www.resultados-futbol.com/grecia2013/grupo1/jornada1",
+		"http://www.resultados-futbol.com/grecia2012/grupo1/jornada1",
+		"http://www.resultados-futbol.com/grecia2010/grupo1/jornada1",
+		"http://www.resultados-futbol.com/grecia2009/grupo1/jornada1",
+		"http://www.resultados-futbol.com/grecia2008/grupo1/jornada1",
+		"http://www.resultados-futbol.com/grecia2007/grupo1/jornada1",
+		"http://www.resultados-futbol.com/grecia2006/grupo1/jornada1",
+		"http://www.resultados-futbol.com/grecia2005/grupo1/jornada1"  
+
+
         ]
     
     rules = (
